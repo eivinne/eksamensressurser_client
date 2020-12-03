9f5d54f945272e0f4c55ec6ba1f82aee5d083ebe
MattiaLerario <mattia@leratechsolutions.com>
2020-12-03 20:08:26 +0100

    cleaned file

--- src/components/ViewOfficesComponent.jsx
+++ src/components/ViewOfficesComponent.jsx
@@ -1,5 +1,3 @@
-/* TODO add functionality for how Offices are viewed */
-import { render } from '@testing-library/react';
 import React from 'react'
 import { ViewOfficesDiv } from '../styles/StyledComponents'
 import ViewOfficeIndividual from './ViewOfficeIndividual'
@@ -7,27 +5,17 @@ import offices from './../data/Offices.json';
 
 const ViewOfficesComponent = () => {
 
-    
-
     return (
-        
         <ViewOfficesDiv>
             <section className="main-banner">Kontorer</section>
             <button>List View</button>
             <ul>
-                <p>Fredrikstad (8 Kontorer)</p>
+                <p>Fredrikstad ({offices.length} Kontorer)</p>
                 {offices.map((office,index)=>{
-                    return <ViewOfficeIndividual key={index} office={office}/>
-
+                return <ViewOfficeIndividual key={index} office={office}/>
                 })}
-                    
-                   
-                    
             </ul>
-         
         </ViewOfficesDiv>
     )}
-    
-
 
 export default ViewOfficesComponent
